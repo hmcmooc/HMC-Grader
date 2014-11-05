c53ae3bc9d80041c15751098923c4dbf1127ec14
plenk <robojeb@gmail.com>
2014-11-05 14:42:41 -0800

    fixed bug in make blank fixed an issue where if the admin ins't in the course it crashes

--- app/grutor/redirects.py
+++ app/grutor/redirects.py
@@ -231,7 +231,7 @@ def grutorMakeBlank(pid, uid):
     p.studentSubmissions[user.username] = StudentSubmissionList()
 
     #create a filepath
-    filepath = getSubmissionPath(c, a, p, user, len(p.studentSubmissions[g.user.username].submissions))
+    filepath = getSubmissionPath(c, a, p, user, len(p.studentSubmissions[user].submissions))
 
     sub = Submission()
     #Initial fields for submission
