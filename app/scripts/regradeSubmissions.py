7f219c7c9a74717856f789a0603a3ede1def1e2f
Jeb Brooks <robojeb@gmail.com>
2015-02-05 15:53:08 -0800

    Added regrade submission scrip

--- app/scripts/regradeSubmissions.py
+++ app/scripts/regradeSubmissions.py
@@ -2,10 +2,12 @@ import sys
 
 from app.scripts.helpers import *
 from app.helpers.autograder import *
+from app.structures.models.user import *
+from app.helpers.autograder import regradeSubmission
 
 if __name__ == "__main__":
-  semester = raw_input("Course Semester: ")
-  courseName = raw_input("Course Name: ")
+  courseName = "CS 5" #raw_input("Course Name: ")
+  semester = "Spring 15" #raw_input("Course Semester: ")
 
   course = getCourse(semester, courseName)
 
@@ -13,7 +15,25 @@ if __name__ == "__main__":
     print "Could not find the course you specified"
     sys.exit(1)
 
-  assignments = [x.name for x in course.name]
-
-  for i, a in enumerate(assignments):
+  for i, a in enumerate(course.assignments):
     print "%d) %s" % (i, a.name)
+
+  index = int(raw_input("Pick an assignment: "))
+
+  assignment = course.assignments[index]
+
+  for i, p in enumerate(assignment.problems):
+    print "%d) %s" % (i, p.name)
+
+  index = int(raw_input("Pick a problem: "))
+
+  problem = assignment.problems[index]
+
+  students = User.objects.filter(courseStudent=course)
+
+  for s in students:
+    print "Student: " + s.username
+    sub = problem.getLatestSubmission(s)
+
+    if sub != None:
+      regradeSubmission.delay(sub)
